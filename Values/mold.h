eb7ff6c91971290d0c9ae1e26c20b043aefe2c28
a <@gmail.com>
2022-07-25 13:59:23 +0200

    Values

---
+++ Values/mold.h
@@ -0,0 +1,49 @@
+/* Mold Values */
+
+// exclusive molds
+
+float binary = 625.81
+float dimensional = 625.81
+float fuego = 911.46
+float monochromatic = 569.66
+float nebula = 569.66
+float angelic = 955.32
+int bloodOath = 2951
+int jade = 4722
+int cryogenic = 10017
+
+
+// real molds
+
+float wood = 1.6
+float stone = 2.56
+float copper = 4.09
+float bronze = 6.55
+float silver = 10.48
+float gold = 16.77
+float platinum = 26.84
+float leSteel = 42.94
+float sapphire = 68.71
+float emerald = 109.95
+float ruby 175.92
+float amethyst = 281.47
+float diamond = 450.35
+float opal = 720.57
+
+// int values from here on out
+
+int uranium = 1153
+int sulfur = 1845
+int crystal = 2951
+int bloodstone = 4722
+int moonstone = 7556
+int topaz = 12089
+int fossil = 18134
+int painite = 27201
+int azure = 40801
+int volcanic = 61202
+int quartz = 91803
+int ammolite = 137704
+int aquamarine = 206556
+int mythril = 309834
+int soulstone = 464752
